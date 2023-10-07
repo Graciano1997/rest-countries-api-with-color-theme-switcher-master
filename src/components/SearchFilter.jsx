1139d0a7b9d437eeb85ee2b5ec2d00be9714b6ab
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-07 02:11:54 +0100

    update filter component

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -7,14 +7,15 @@ import style from '../style/countries.module.css';
 const SearchFilter = () => {
   const dispatch = useDispatch();
   const handlerChange = (region) => {
-    if (region !== 'all') {
-      dispatch(resetAllCountries());
-      dispatch(getCountriesRegion(region));
-    } else {
+    if (region === 'all') {
       dispatch(resetAllCountries());
       dispatch(getCountries());
+    } else if (region !== '') {
+      dispatch(resetAllCountries());
+      dispatch(getCountriesRegion(region));
     }
   };
+
   const handlerKeyDown = (name) => {
     if (name !== '') {
       dispatch(resetAllCountries());
@@ -34,12 +35,13 @@ const SearchFilter = () => {
             handlerChange(el.target.value);
           }}
         >
-          <option value="all">Filter By Region</option>
+          <option value="">Filter By Region</option>
           <option value="africa">Africa</option>
           <option value="america">America</option>
           <option value="asia">Asia</option>
           <option value="europe">Europe</option>
           <option value="oceania">Oceania</option>
+          <option value="all">world</option>
         </select>
       </div>
     </section>
